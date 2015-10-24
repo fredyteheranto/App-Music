95a4715e878d4ddf1535d1c5d96304f5c4bb3e65
fredy saul teheran tovar <fredyteheran91@gmail.com>
2015-10-23 19:57:20 -0500

    edit file redme

---
+++ public/dev/js/script.js
@@ -0,0 +1,35 @@
+angular.module('asideApp', ['ui.bootstrap', 'ngAside'])
+  .controller('MainCtrl', function($scope, $aside) {
+    
+    $scope.asideState = {
+      open: false
+    };
+    
+    $scope.openAside = function(position, backdrop) {
+      $scope.asideState = {
+        open: true,
+        position: position
+      };
+      
+      function postClose() {
+        $scope.asideState.open = false;
+      }
+      
+      $aside.open({
+        templateUrl: 'aside.html',
+        placement: position,
+        size: 'sm',
+        backdrop: backdrop,
+        controller: function($scope, $modalInstance) {
+          $scope.ok = function(e) {
+            $modalInstance.close();
+            e.stopPropagation();
+          };
+          $scope.cancel = function(e) {
+            $modalInstance.dismiss();
+            e.stopPropagation();
+          };
+        }
+      }).result.then(postClose, postClose);
+    }
+  });
